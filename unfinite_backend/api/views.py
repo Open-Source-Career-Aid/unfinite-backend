f99a86bb0e337fa1e24c05a107a5746b314ae12c
pmcurtin <peter_curtin@brown.edu>
2023-02-22 16:33:06 -0500

    change how csrfToken is sent

--- unfinite_backend/api/views.py
+++ unfinite_backend/api/views.py
@@ -18,11 +18,11 @@ def requires_authentication(func):
 
     return wrap
 
-@ensure_csrf_cookie
+@csrf_exempt
 def get_csrf_cookie(request):
     # a nice little function that the front-end can use to get a CSRF token if
     # it needs to. Might not be needed.
-    return JsonResponse({}, status=200)
+    return JsonResponse({'csrfToken': get_token(request)}, status=200)
 
 @require_POST
 def login_view(request):
