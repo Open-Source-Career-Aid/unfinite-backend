2ceeccd7a996dbe7cef8cf9f819babd996ec9fe5
pmcurtin <peter_curtin@brown.edu>
2023-02-27 19:21:08 -0500

    change root

--- unfinite_backend/unfinite_backend/settings.py
+++ unfinite_backend/unfinite_backend/settings.py
@@ -18,7 +18,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_ROOT = os.path.join(BASE_DIR, str(os.getenv('REL_STATIC_ROOT')))
+STATIC_ROOT = str(os.getenv('STATIC_ROOT'))
 
 STATICFILES_DIRS = [BASE_DIR / 'unfinitefront/unfinitebeta/build/static']
 #print(STATICFILE_DIRS)
