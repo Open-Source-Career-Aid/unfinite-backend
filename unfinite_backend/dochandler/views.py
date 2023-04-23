240be26832a397bd21cd093a802c1d65eed6bd17
pmcurtin <peter_curtin@brown.edu>
2023-04-23 17:55:53 -0400

    chinmay's updates

--- unfinite_backend/dochandler/views.py
+++ unfinite_backend/dochandler/views.py
@@ -19,7 +19,7 @@ openai.api_key = settings.OPENAI_API_KEY
 pinecone.init(api_key=settings.PINECONE_KEY, environment="us-central1-gcp")
 index = pinecone.Index('unfinite-embeddings')
 
-special_prompts = {1: 'Simplify'}
+special_prompts = {1: 'Simplify for someone who isn\'t knowledgeable in the field', 2: 'Dumbsplain for a 5 year old kid', 3: 'Talk extremely technical as you would to an academic'}
 
 # a functuon that uses regex to verify that a text is a url
 def is_url(text):
